2c42c26189635ce19b29ae28ff337725477b7188
ShlomoNus <shlomonuswork@gmail.com>
2024-05-01 17:07:48 +0300

    feature: 1.add CustomRequest 2.add to hander result type. 3. handler type support sync and async function.

--- src/backend.ts
+++ src/backend.ts
@@ -1,31 +1,37 @@
 import {
-    Router,
-    Request,
-    Response,
-    RequestHandler as Middleware,
+  Router,
+  Request,
+  Response,
+  RequestHandler as Middleware,
 } from 'express';
 import { AnyType } from 'sn-types-general';
 
-export type Handler<TBody = AnyType, TParams = unknown, Tquery = unknown> = (
-    req: Request<TParams, unknown, TBody, Tquery>,
-    res: Response
-) => AnyType;
+type CustomRequest<
+  TBody = AnyType,
+  TParams = AnyType,
+  Tquery = AnyType
+> = Request<TParams, unknown, TBody, Tquery>;
+
+export type Handler< TResult=AnyType,TBody = AnyType, TParams = AnyType, Tquery = AnyType> = (
+  req: CustomRequest<TParams, TBody, Tquery>,
+  res: Response
+) => TResult | Promise<TResult>;
 
 type Method =
-    | 'get'
-    | 'head'
-    | 'post'
-    | 'put'
-    | 'delete'
-    | 'connect'
-    | 'options'
-    | 'trace'
-    | 'patch'
-    | 'use';
+  | 'get'
+  | 'head'
+  | 'post'
+  | 'put'
+  | 'delete'
+  | 'connect'
+  | 'options'
+  | 'trace'
+  | 'patch'
+  | 'use';
 
 export type Route = {
-    method: Method;
-    path: string;
-    middleware: Middleware[];
-    handler: Handler | Router;
+  method: Method;
+  path: string;
+  middleware: Middleware[];
+  handler: Handler | Router;
 };
